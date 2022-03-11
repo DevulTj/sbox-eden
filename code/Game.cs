6e4e1c4d1070125e337c281155a08b21f1c4afc1
Tony Ferguson <devultj@gmail.com>
2022-03-11 00:46:00 +0000

    Created .addon, as well as template Game/Player

---
+++ code/Game.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+using Sandbox;
+using System;
+using System.Linq;
+
+namespace Eden;
+
+/// <summary>
+/// This is your game class. This is an entity that is created serverside when
+/// the game starts, and is replicated to the client. 
+/// 
+/// You can use this to create things like HUDs and declare which player class
+/// to use for spawned players.
+/// </summary>
+public partial class Game : Sandbox.Game
+{
+	public Game()
+	{
+	}
+
+	/// <summary>
+	/// A client has joined the server. Make them a pawn to play with
+	/// </summary>
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var player = new Player();
+		client.Pawn = player;
+		player.Respawn();
+
+		var spawnPoints = Entity.All.OfType<SpawnPoint>();
+		var randomSpawnPoint = spawnPoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		if ( randomSpawnPoint is null )
+			return;
+			
+		var tx = randomSpawnPoint.Transform;
+		tx.Position = tx.Position + Vector3.Up * 50.0f;
+		player.Transform = tx;
+	}
+}
---
+++ code/player/Player.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+using Sandbox;
+using System;
+using System.Linq;
+
+namespace Eden;
+
+public partial class Player : Sandbox.Player
+{
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	public override void Spawn()
+	{
+		base.Spawn();
+	}
+
+	public override void Respawn()
+	{
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		Controller = new WalkController();
+		Animator = new StandardPlayerAnimator();
+		CameraMode = new FirstPersonCamera();
+
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		base.Respawn();
+	}
+
+	/// <summary>
+	/// Called every tick, clientside and serverside.
+	/// </summary>
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+	}
+
+	/// <summary>
+	/// Called every frame on the client
+	/// </summary>
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+	}
+}
