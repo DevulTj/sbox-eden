86c343595a3b006851e3ac239c2f89be080eb0bd
Tony Ferguson <devultj@gmail.com>
2022-03-11 16:26:13 +0000

    Add Thirst vital

---
+++ code/ui/hud/vitals/ThirstVital.cs
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+using Sandbox;
+using Sandbox.UI;
+
+namespace Eden;
+
+[UseTemplate( "ui/hud/vitals/vitalentry.html" )]
+public partial class ThirstVital : VitalEntry
+{
+	public string IconPath => "/ui/hud/vitals/thirst.png";
+
+	public ThirstVital() : base()
+	{
+		Icon.Style.SetBackgroundImage( IconPath );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		BoundValue = 30f;
+	}
+}
