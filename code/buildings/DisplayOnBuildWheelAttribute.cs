0b1ba02b2c9a91933ba8394ce029e322406f6f86
Alex <alex@gu3.me>
2022-03-13 13:50:13 +0000

    Base building type, add DisplayOnBuildWheel attribute to display things on the build wheel

---
+++ code/buildings/DisplayOnBuildWheelAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+using Sandbox;
+
+namespace Eden;
+
+public class DisplayOnBuildWheelAttribute : LibraryAttribute
+{
+	public DisplayOnBuildWheelAttribute( string title, string icon )
+	{
+		this.Title = title;
+		this.Icon = icon;
+	}
+}
---
+++ code/buildings/Stairs.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+namespace Eden;
+
+[DisplayOnBuildWheel( "Stairs", "ui/building/stairs.png" )]
+public class Stairs : BaseBuilding
+{
+}
