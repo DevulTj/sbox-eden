937d74ec0234649344ab19a3e86ac95dafd141bb
Alex <alex@gu3.me>
2022-03-13 14:03:39 +0000

    BaseBuilding method stubs

--- code/buildings/BaseBuilding.cs
+++ code/buildings/BaseBuilding.cs
@@ -1,14 +1,27 @@
 ﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
 // without permission of its author (insert_email_here)
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Eden;
 
 public class BaseBuilding
 {
+	public virtual void PlaceBuilding()
+	{
+
+	}
+
+	public virtual Vector3 FindSnapPoint()
+	{
+		return Vector3.Zero;
+	}
+
+	public virtual bool IsPlacementValid()
+	{
+		return true;
+	}
+
+	public virtual bool CanAfford( Player player )
+	{
+		return true;
+	}
 }
