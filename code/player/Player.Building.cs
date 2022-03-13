7b13f7b574270b46c8bbe23cc4ac0e43e0036dc3
Alex <alex@gu3.me>
2022-03-13 13:25:29 +0000

    Temp implementation: show buildings in build menu

--- code/player/Player.Building.cs
+++ code/player/Player.Building.cs
@@ -2,6 +2,7 @@
 // without permission of its author (insert_email_here)
 
 using Sandbox;
+using System.Collections.Generic;
 
 namespace Eden;
 
@@ -17,11 +18,20 @@ partial class Player
 		if ( Input.Pressed( InputButton.Menu ) && buildWheel == null )
 		{
 			buildWheel = RadialWheel.Create();
-			buildWheel.AddOption( "Test 1", "tools/images/common/icon_info.png", () => Log.Trace( "1" ) );
-			buildWheel.AddOption( "Test 2", "tools/images/common/icon_info.png", () => Log.Trace( "2" ) );
-			buildWheel.AddOption( "Test 3", "tools/images/common/icon_info.png", () => Log.Trace( "3" ) );
-			buildWheel.AddOption( "Test 4", "tools/images/common/icon_info.png", () => Log.Trace( "4" ) );
-			buildWheel.AddOption( "Test 5", "tools/images/common/icon_info.png", () => Log.Trace( "5" ) );
+
+			List<string> buildingTypes = new( new[] {
+				"door",
+				"floor",
+				"roof",
+				"stairs",
+				"wall",
+				"window"
+			} );
+
+			buildingTypes.ForEach( t =>
+			{
+				buildWheel.AddOption( t.ToTitleCase(), $"ui/building/{t}.png", () => Log.Trace( $"Build {t}" ) );
+			} );
 		}
 
 		if ( Input.Released( InputButton.Menu ) )
