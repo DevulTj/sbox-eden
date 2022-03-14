d8283604c4139f6c1a0a01a80db59e5559608ee2
Tony Ferguson <devultj@gmail.com>
2022-03-14 17:46:45 +0000

    Page system is functional

--- code/ui/inventory/MainMenuPanel.cs
+++ code/ui/inventory/MainMenuPanel.cs
@@ -3,6 +3,7 @@
 
 using Sandbox;
 using Sandbox.UI;
+using System.Collections.Generic;
 
 namespace Eden;
 
@@ -11,17 +12,75 @@ public partial class MainMenuPanel : Panel
 {
 	public bool IsOpen { get; protected set; } = false;
 
+	public List<MainMenuPage> Pages { get; set; } = new();
+	public List<Panel> SectionDots { get; set; } = new();
+
+	public MainMenuPage CurrentPage { get; set; }
+	public int CurrentPageIndex { get; set; }
+
+
 	// @ref
 	public Panel PageLayout { get; set; }
+	// @ref
+	public Panel SectionDotsPanel { get; set; }
+	// @ref
+	public Label CurrentPageLabel { get; set; }
+	// @ref
+	public Label PreviousPageLabel { get; set; }
+	// @ref
+	public Label NextPageLabel { get; set; }
 
 	public MainMenuPanel()
 	{
-		BindClass( "open", () => IsOpen );
 	}
+
+	protected void Setup()
+	{
+		SectionDotsPanel.DeleteChildren( true );
+		PageLayout.DeleteChildren( true );
+
+		Pages = new();
+		SectionDots = new();
+		CurrentPage = null;
+
+		BindClass( "open", () => IsOpen );
+
+		// 0
+		AddPage( new SkillsPage() );
+		// 1
+		AddPage( new BackpackPage() );
+		// 2
+		AddPage( new CraftingPage() );
+		// @TODO: better way to handle the default page?
+		SetPageIndex( 1 );
+	}
+
+	public int GetNextPageIndex()
+	{
+		var index = CurrentPageIndex + 1;
+		if ( index > Pages.Count - 1 )
+			index = 0;
+
+		return index;
+	}
+
+	public int GetPreviousPageIndex()
+	{
+		var index = CurrentPageIndex - 1;
+		if ( index < 0 )
+			index = Pages.Count - 1;
+
+		return index;
+	}
+
+	public MainMenuPage GetNextPage() => Pages[GetNextPageIndex()];
+	public MainMenuPage GetPreviousPage() => Pages[GetPreviousPageIndex()];
 
 	protected override void PostTemplateApplied()
 	{
 		base.PostTemplateApplied();
+
+		Setup();
 	}
 
 	public override void Tick()
@@ -33,13 +92,43 @@ public partial class MainMenuPanel : Panel
 	public void BuildInput( InputBuilder input )
 	{
 		if ( input.Pressed( InputButton.Score ) )
-		{
 			IsOpen = !IsOpen;
+
+		if ( input.Released( InputButton.Menu ) )
+		{
+			SetPageIndex( GetPreviousPageIndex() );
+			input.ClearButton( InputButton.Menu );
+		}
+
+		if ( input.Released( InputButton.Use ) )
+		{
+			SetPageIndex( GetNextPageIndex() );
+			input.ClearButton( InputButton.Use );
 		}
 	}
 
-	public void SetActivePage( MainMenuPage page )
+	public void SetPageIndex( int index )
 	{
-		//
+		var page = Pages[index];
+		CurrentPage = page;
+		CurrentPageIndex = index;
+
+		Log.Info( $"current: {index}, prev: {GetPreviousPageIndex()}, next: {GetNextPageIndex()}" );
+
+		CurrentPageLabel.Text = page.PageName;
+		PreviousPageLabel.Text = GetPreviousPage().PageName;
+		NextPageLabel.Text = GetNextPage().PageName;
+	}
+
+	public void AddPage( MainMenuPage page )
+	{
+		page.Parent = PageLayout;
+
+		Pages.Add( page );
+
+		page.BindClass( "show", () => CurrentPage == page );
+
+		var panel = SectionDotsPanel.AddChild<Panel>( "section" );
+		SectionDots.Add( panel );
 	}
 }
