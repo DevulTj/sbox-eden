69b0f0da0aff774cea721d7fa172625c76e0df3c
Alex <alex@gu3.me>
2022-03-14 13:37:33 +0000

    Add snap points & custom modeldoc snap points node to each model

---
+++ code/buildings/ModelSnapPoints.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+using Sandbox;
+using System.Text.Json.Serialization;
+
+namespace Eden;
+
+/// <summary>
+/// Determines which attachments are used for snapping.
+/// 
+/// <para>
+///	Snapped buildings will be rotated in the forward (X) direction for each attachment.
+/// </para>
+/// </summary>
+[ModelDoc.GameData( "eden_snap_points", AllowMultiple = true )]
+public class ModelSnapPoints
+{
+	[JsonPropertyName( "snap_points" ), FGDType( "model_attachment" )]
+	public string[] SnapPoints { get; set; }
+}
