b40faf28ecac19504e5e443025e601c05ac9ab6b
Alex <alex@gu3.me>
2022-03-15 13:02:07 +0000

    Primitive building snapping

--- code/weapons/Blueprint.cs
+++ code/weapons/Blueprint.cs
@@ -2,6 +2,7 @@
 // without permission of its author (insert_email_here)
 
 using Sandbox;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Eden;
@@ -28,17 +29,20 @@ partial class Blueprint : Weapon
 			DeleteBuildWheel();
 
 		if ( Input.Pressed( InputButton.Attack1 ) )
-			PlaceBuilding( selectedAsset.Id );
+		{
+			var targetTransform = GetSnappedTransform( new Transform( TraceForward( Owner ).EndPosition, Rotation.Identity ) );
+			PlaceBuilding( selectedAsset.Id, targetTransform.Position, targetTransform.Rotation );
+		}
 	}
 
 	[ServerCmd( "eden_building_place" )]
-	public static void PlaceBuilding( int assetId )
+	public static void PlaceBuilding( int assetId, Vector3 position, Rotation rotation )
 	{
 		var asset = Asset.FromId<BuildingAsset>( assetId );
 
 		var building = new BuildingEntity();
-		building.Position = TraceForward( ConsoleSystem.Caller.Pawn ).EndPosition;
-		building.Rotation = Rotation.Identity;
+		building.Position = position;
+		building.Rotation = rotation;
 		building.Model = asset.BuildingModel;
 		building.SetupPhysicsFromModel( PhysicsMotionType.Static );
 	}
@@ -51,7 +55,8 @@ partial class Blueprint : Weapon
 		{
 			ghostEntity = new GhostEntity()
 			{
-				Owner = Owner
+				Owner = Owner,
+				Blueprint = this
 			};
 		}
 	}
@@ -64,9 +69,75 @@ partial class Blueprint : Weapon
 			ghostEntity?.Delete();
 	}
 
-	private static TraceResult TraceForward( Entity entity )
+	private static TraceResult TraceForward( Entity entity, float distance = 512f )
 	{
-		return Trace.Ray( entity.EyePosition, entity.EyePosition + entity.EyeRotation.Forward * 512 ).Ignore( entity ).Run();
+		return Trace.Ray( entity.EyePosition, entity.EyePosition + entity.EyeRotation.Forward * distance ).Ignore( entity ).Run();
+	}
+
+	private List<Transform> GetNearestSnapPoints()
+	{
+		var trace = TraceForward( Owner, 128f );
+		var overlaps = Entity.FindInSphere( trace.EndPosition, 128f ).OfType<BuildingEntity>();
+
+		var list = new List<Transform>();
+
+		foreach ( var overlap in overlaps )
+		{
+			if ( !overlap.IsValid )
+				continue;
+
+			var datas = overlap.Model?.GetData<ModelSnapPoints[]>();
+
+			if ( datas == null )
+				continue;
+
+			var data = datas[0];
+			var snapPoints = data.SnapPoints;
+			var snapTransforms = snapPoints.Select( attachment => overlap.GetAttachment( attachment ) );
+			snapTransforms.ToList().ForEach( snapPoint => list.Add( snapPoint ?? default ) );
+		}
+
+		return list;
+	}
+
+	[Event.Frame]
+	public void OnFrame()
+	{
+		foreach ( var snapPoint in GetNearestSnapPoints() )
+		{
+			DebugOverlay.Sphere( snapPoint.Position, 4f, Color.Cyan, false );
+		}
+
+		if ( ghostEntity == null )
+			return;
+
+		foreach ( var snapPoint in ghostEntity.GetSnapPoints() )
+		{
+			DebugOverlay.Sphere( snapPoint.Position, 4f, Color.Green, false );
+		}
+	}
+
+	private Transform GetSnappedTransform( Transform transform )
+	{
+		var forwardTracePosition = TraceForward( Owner, 128f ).EndPosition;
+
+		var nearestSnapPoint = GetNearestSnapPoints().OrderBy( x => x.Position.Distance( forwardTracePosition ) ).FirstOrDefault();
+
+		if ( nearestSnapPoint == default )
+			return transform;
+
+		if ( forwardTracePosition.Distance( nearestSnapPoint.Position ) < 128 )
+		{
+			var localOther = ghostEntity.Transform.ToLocal( nearestSnapPoint );
+			var localGhostSnapPoints = ghostEntity.GetSnapPoints().Select( x => ghostEntity.Transform.ToLocal( x ) );
+			var localClosestSnapPoint = localGhostSnapPoints.OrderBy( x => x.Position.Distance( localOther.Position ) ).First();
+
+			DebugOverlay.ScreenText( 0, localClosestSnapPoint.Position.ToString() );
+
+			return nearestSnapPoint.WithPosition( nearestSnapPoint.Position - localClosestSnapPoint.Position );
+		}
+
+		return transform;
 	}
 
 	private void CreateBuildWheel()
