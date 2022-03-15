38076dde76174dc130495c6a9118cca815fcdbd9
Tony Ferguson <devultj@gmail.com>
2022-03-15 13:08:50 +0000

    Add CraftingRecipe.IsValid

--- code/crafting/CraftingRecipe.cs
+++ code/crafting/CraftingRecipe.cs
@@ -5,6 +5,8 @@ namespace Eden;
 
 public struct CraftingRecipe
 {
+	public bool IsValid => ItemAsset.Classes.ContainsKey( ItemId ) && Amount > 0;
+
 	public string ItemId { get; set; }
-	public int Amount { get; set; }
+	public int Amount { get; set; } = 0;
 }
