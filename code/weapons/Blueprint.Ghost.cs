eee84e40618cd9f6fa2e6e5f0a213989f1b64fe3
Alex <alex@gu3.me>
2022-03-15 13:02:07 +0000

    Primitive building snapping

--- code/weapons/Blueprint.Ghost.cs
+++ code/weapons/Blueprint.Ghost.cs
@@ -2,15 +2,17 @@
 // without permission of its author (insert_email_here)
 
 using Sandbox;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Eden;
-
 
 partial class Blueprint : Weapon
 {
 	private partial class GhostEntity : ModelEntity
 	{
+		internal Blueprint Blueprint { get; set; }
+
 		public override void Spawn()
 		{
 			base.Spawn();
@@ -25,8 +27,34 @@ partial class Blueprint : Weapon
 		[Event.Frame]
 		public void OnFrameUpdate()
 		{
-			Position = TraceForward( Owner ).EndPosition;
+			var snappedTransform = TraceForward( Owner, 128f ).EndPosition;
+			Position = snappedTransform;
 			Rotation = Rotation.Identity;
+		}
+
+		[Event.PreRender]
+		public void OnPreRender()
+		{
+			var snappedTransform = Blueprint?.GetSnappedTransform( new Transform( TraceForward( Owner ).EndPosition, Rotation.Identity ) ) ?? default;
+			SceneObject.Transform = snappedTransform;
+		}
+
+		// TODO: bit of a shit place to put this
+		internal List<Transform> GetSnapPoints()
+		{
+			var list = new List<Transform>();
+
+			var datas = Model?.GetData<ModelSnapPoints[]>();
+
+			if ( datas == null )
+				return new();
+
+			var data = datas[0];
+			var snapPoints = data.SnapPoints;
+			var snapTransforms = snapPoints.Select( attachment => GetAttachment( attachment ) );
+			snapTransforms.ToList().ForEach( snapPoint => list.Add( snapPoint ?? default ) );
+
+			return list;
 		}
 	}
 }
