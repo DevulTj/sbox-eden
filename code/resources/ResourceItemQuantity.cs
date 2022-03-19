e64c544064b791c54c9017be571f60a6e34f7187
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-03-19 00:07:58 +0000

    ResourceItemQuantity

---
+++ code/resources/ResourceItemQuantity.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+namespace Eden;
+
+public struct ResourceItemQuantity
+{
+	public string ItemAsset { get; set; }
+	public string TotalQuantity { get; set; }
+}
