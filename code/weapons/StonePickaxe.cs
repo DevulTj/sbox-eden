623835bbf5e36d406c6dc775ff9d9c0badd124a4
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-03-21 21:50:04 +0000

    Add a stone pickaxe

---
+++ code/weapons/StonePickaxe.cs
@@ -0,0 +1,52 @@
+// Copyright (c) 2022 Ape Tavern, do not share, re-distribute or modify
+// without permission of its author (insert_email_here)
+
+using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Eden;
+
+[Library( "eden_pickaxe_stone", Title = "Stone Pickaxe", Spawnable = false )]
+partial class StonePickaxe : MeleeWeapon
+{
+	public override string ViewModelPath => "models/tools/hatchet/hatchet_v.vmdl";
+	public override float PrimaryRate => 1.0f;
+
+	public override Dictionary<ResourceType, int> ResourceYield => new()
+	{
+		{ ResourceType.Wood, 0 },
+		{ ResourceType.Stone, 1 }
+	};
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Model = Model.Load( "models/tools/hatchet/hatchet_w.vmdl" );
+	}
+
+	public override void OnCarryDrop( Entity dropper )
+	{
+	}
+
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		anim.SetAnimParameter( "holdtype", HoldType.Item.ToInt() );
+		anim.SetAnimParameter( "holdtype_handedness", HoldHandedness.RightHand.ToInt() );
+		anim.SetAnimParameter( "holdtype_pose_hand", 0.07f );
+		anim.SetAnimParameter( "holdtype_attack", 1 );
+	}
+
+	public override void CreateViewModel()
+	{
+		ViewModelData = new()
+		{
+			SwingInfluence = 0.03f,
+			Offset = new Vector3( 1f, -1f, -4f ),
+		};
+
+		base.CreateViewModel();
+	}
+}
